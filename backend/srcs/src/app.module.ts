e7447054722ba389b53645344eac4ef8eb162124
mtellami <moadtell@gmail.com>
2023-09-04 19:44:23 +0100

    connect to database

--- backend/srcs/src/app.module.ts
+++ backend/srcs/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [
+		TypeOrmModule.forRoot({
+			type: 'postgres',
+			host: 'localhost', // change it to database container name when run on docker
+			port: 5432,
+			username: 'mtellami',
+			password: 'password',
+			database: 'tranc',
+			entities: [],
+			synchronize: true,
+		}),
+	],
   controllers: [AppController],
   providers: [AppService],
 })
