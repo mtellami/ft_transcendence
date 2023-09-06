d7ae008ad29d130ba3b1134d31eba043d28f5411
mtellami <moadtell@gmail.com>
2023-09-06 17:22:12 +0100

    rebuild

--- backend/srcs/src/users/users.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ContollersController } from './contollers/contollers.controller';
-import { ServicesService } from './services/services.service';
-
-@Module({
-  controllers: [ContollersController],
-  providers: [ServicesService]
-})
-export class UsersModule {}
